9ca2ecffdb8b91abbc99643723aca513de3c6064
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-06-20 15:57:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+
+document.writeln("You are using "+navigator.appCodeName);  
+document.writeln(" "+navigator.appName);  
+document.writeln(" "+navigator.appVersion);
